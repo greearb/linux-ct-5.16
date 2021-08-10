35161701b43b4923ce2ae687073666ab09fa2c27
Ben Greear <greearb@candelatech.com>
2021-08-10 06:41:51 -0700

    mac80211: allow disabling 160mhz in station mode.
    For better test coverage, and maybe working around AP
    bugs.  Might be a way to decrease station power usage too
    if you are on a tight power budget.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- net/mac80211/mlme.c
+++ net/mac80211/mlme.c
@@ -644,6 +644,27 @@ static void ieee80211_add_vht_ie(struct ieee80211_sub_if_data *sdata,
 	ieee80211_ie_build_vht_cap(pos, &vht_cap, cap);
 }
 
+void ieee80211_adjust_he_cap(struct ieee80211_sta_he_cap* my_cap,
+			     const struct ieee80211_sta_he_cap* he_cap,
+			     struct ieee80211_sub_if_data *sdata)
+{
+	memcpy(my_cap, he_cap, sizeof(*my_cap));
+
+	if (sdata->u.mgd.flags & IEEE80211_STA_DISABLE_160MHZ) {
+		my_cap->he_cap_elem.phy_cap_info[0] &=
+			~IEEE80211_HE_PHY_CAP0_CHANNEL_WIDTH_SET_160MHZ_IN_5G;
+		my_cap->he_mcs_nss_supp.rx_mcs_160 = 0xffff;
+		my_cap->he_mcs_nss_supp.tx_mcs_160 = 0xffff;
+		pr_info("adjust-he-capp, disabling 160Mhz.");
+	}
+	if (sdata->u.mgd.flags & IEEE80211_STA_DISABLE_80P80MHZ) {
+		my_cap->he_cap_elem.phy_cap_info[0] &=
+			~IEEE80211_HE_PHY_CAP0_CHANNEL_WIDTH_SET_80PLUS80_MHZ_IN_5G;
+		my_cap->he_mcs_nss_supp.rx_mcs_80p80 = 0xffff;
+		my_cap->he_mcs_nss_supp.tx_mcs_80p80 = 0xffff;
+	}
+}
+
 /* This function determines HE capability flags for the association
  * and builds the IE.
  */
@@ -653,6 +674,7 @@ static void ieee80211_add_he_ie(struct ieee80211_sub_if_data *sdata,
 {
 	u8 *pos;
 	const struct ieee80211_sta_he_cap *he_cap = NULL;
+	struct ieee80211_sta_he_cap my_cap;
 	struct ieee80211_chanctx_conf *chanctx_conf;
 	u8 he_cap_size;
 	bool reg_cap = false;
@@ -671,17 +693,19 @@ static void ieee80211_add_he_ie(struct ieee80211_sub_if_data *sdata,
 	if (!he_cap || !reg_cap)
 		return;
 
+	ieee80211_adjust_he_cap(&my_cap, he_cap, sdata);
+
 	/*
 	 * TODO: the 1 added is because this temporarily is under the EXTENSION
 	 * IE. Get rid of it when it moves.
 	 */
 	he_cap_size =
-		2 + 1 + sizeof(he_cap->he_cap_elem) +
-		ieee80211_he_mcs_nss_size(&he_cap->he_cap_elem) +
-		ieee80211_he_ppe_size(he_cap->ppe_thres[0],
-				      he_cap->he_cap_elem.phy_cap_info);
+		2 + 1 + sizeof(my_cap.he_cap_elem) +
+		ieee80211_he_mcs_nss_size(&my_cap.he_cap_elem) +
+		ieee80211_he_ppe_size(my_cap.ppe_thres[0],
+				      my_cap.he_cap_elem.phy_cap_info);
 	pos = skb_put(skb, he_cap_size);
-	ieee80211_ie_build_he_cap(sdata, pos, he_cap, pos + he_cap_size);
+	ieee80211_ie_build_he_cap(sdata, pos, &my_cap, pos + he_cap_size);
 
 	ieee80211_ie_build_he_6ghz_cap(sdata, skb);
 }
@@ -4979,18 +5003,21 @@ ieee80211_verify_sta_he_mcs_support(struct ieee80211_sub_if_data *sdata,
 	const struct ieee80211_sta_he_cap *sta_he_cap =
 		ieee80211_get_he_iftype_cap(sband,
 					    ieee80211_vif_type_p2p(&sdata->vif));
+	struct ieee80211_sta_he_cap my_cap;
 	u16 ap_min_req_set;
 	int i;
 
 	if (!sta_he_cap || !he_op)
 		return false;
 
+	ieee80211_adjust_he_cap(&my_cap, sta_he_cap, sdata);
+
 	ap_min_req_set = le16_to_cpu(he_op->he_mcs_nss_set);
 
 	/* Need to go over for 80MHz, 160MHz and for 80+80 */
 	for (i = 0; i < 3; i++) {
 		const struct ieee80211_he_mcs_nss_supp *sta_mcs_nss_supp =
-			&sta_he_cap->he_mcs_nss_supp;
+			&my_cap.he_mcs_nss_supp;
 		u16 sta_mcs_map_rx =
 			le16_to_cpu(((__le16 *)sta_mcs_nss_supp)[2 * i]);
 		u16 sta_mcs_map_tx =
